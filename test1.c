2ca838ed716cfbdb691a0a1415d295c69593379b
SJ00-03 <seongjun000313@gmail.com>
2025-12-29 09:07:15 +0900

    1st commit

--- test1.c
+++ test1.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
+
 int main(void)
 
 {
-    printf("Be Happy\n");
-    printf("12345678901234567890\n");
-    printf("My\tfriend\n");
-    printf("Goot\bd\tchance\n");
-    printf("Cow\rW\a\n");
-    
-    printf("%d\n", 10);
-    printf("%lf\n", 3.4);
-    printf("%.1lf\n", 3.45);
-    printf("%.10lf\n", 3.4);
+    int a = 20, b = 3;
+    double res;
 
-    printf("%d과 %d의 합은 %d입니다.\n", 10, 20, 10+20);
-    printf("%.1lf - %.1lf = %.1lf\n", 3.4, 1.2, 3.4 - 1.2);
-    printf("%.3lf + %.3lf = %.3lf\n", 3.1234, 6.1326, 3.1234+6.1326);
+    res = ((double)a)/((double)b);
+    printf("a = %d, b = %d\n", a, b);
+    printf("a / b의 결과: %lf\n", res);
+
+    a = (int)res;
+    printf("(int) %lf의 결과: %d\n", res, a);
+
 
     
     return 0;
-    
 }
